13d7658e893330c3642d6fefb599c060315dc851
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-27 15:39:51 -0300

    add bcrypt for user password

--- src/presentation/services/auth.service.ts
+++ src/presentation/services/auth.service.ts
@@ -1,17 +1,38 @@
+import { bcryptAdapter } from "../../config";
 import { UserModel } from "../../data";
-import { CustomError, RegisterUserDto } from "../../domain";
+import { CustomError, RegisterUserDto, UserEntity } from "../../domain";
 
 export class AuthService {
     //Dependency Injection
     constructor() {}
 
     public async registerUser(registerUserDto: RegisterUserDto) {
+
         const existUser = await UserModel.findOne({email: registerUserDto.email});
-
         if (existUser ) throw CustomError.badRequest('Email already exist');
+        
+        try {
 
-        return 'ok!'
+            const user = new UserModel(registerUserDto);
+            
+            // Encriptar la contraseña
+            user.password = bcryptAdapter.hash(registerUserDto.password);
+
+            await user.save();
+            // JWT -> para mantener la autenticación del usuario
+            //Email de confirmación
+            
+            const { password, ...userEntity } = UserEntity.fromObject(user);
+            
+            return {
+                user: userEntity,
+                token: 'ABC'
+            };
+
+        } catch (error) {
+            throw CustomError.internalServer(`${error}`);
+        }
+
     }
-
 
 }
