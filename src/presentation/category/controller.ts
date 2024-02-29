39802f5d024347d5595651fc40ddab1ce24c9aa8
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-02-29 17:07:54 -0300

    add pagination in get categories

--- src/presentation/category/controller.ts
+++ src/presentation/category/controller.ts
@@ -1,8 +1,11 @@
 import { Response, Request } from "express";
-import { CreateCategoryDto, CustomError } from "../../domain";
+import { CreateCategoryDto, CustomError, PaginationDto } from "../../domain";
+import { CategoryService } from "../services/category.service";
 
 export class CategoryController {
-    constructor(){}
+    constructor(
+        private readonly categoryService: CategoryService,
+    ){ }
     private handleError = (error: unknown, res: Response) => {
         if (error instanceof CustomError) {
             return res.status(error.statusCode).json({ error: error.message });
@@ -18,15 +21,20 @@ export class CategoryController {
         const [ error, createCategoryDto ] = CreateCategoryDto.create( req.body );
         if ( error ) return res.status( 400 ).json( { error } );
     
-    
-        // this.categoryService.createCategory( createCategoryDto!, req.body.user )
-        //   .then( category => res.status( 201 ).json( category ) )
-        //   .catch( error => this.handleError( error, res ) );
-    
-    
+        this.categoryService.createCategory( createCategoryDto!, req.body.user )
+          .then( category => res.status( 201 ).json( category ) )
+          .catch( error => this.handleError( error, res ) );    
       };
     
-    getCategory = async(req: Request, res: Response) => {
-        res.json('hola')
-    }
+      getCategories = async ( req: Request, res: Response ) => {
+
+        const { page = 1, limit = 10 } = req.query;
+        const [ error, paginationDto ] = PaginationDto.create( +page, +limit );
+        if ( error ) return res.status(400).json({ error });
+        
+        this.categoryService.getCategories( paginationDto! )
+          .then( categories => res.json( categories ))
+          .catch( error => this.handleError( error, res ) );
+    
+      };
 }
--- src/presentation/category/routes.ts
+++ src/presentation/category/routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { CategoryController } from './controller';
+import { AuthMiddleware } from '../middlewares/auth.middleware';
+import { CategoryService } from '../services/category.service';
 
 export class CategoryRoutes {
 
@@ -7,11 +9,12 @@ export class CategoryRoutes {
   static get routes(): Router {
 
     const router = Router();
-    const controller = new CategoryController();
-    
+    const categoryService = new CategoryService();
+    const controller = new CategoryController(categoryService);
+
     // Definir las rutas
-    router.get('/', controller.getCategory);
-    router.post('/', controller.createCategory);
+    router.get('/', controller.getCategories);
+    router.post('/', [AuthMiddleware.validateJWT], controller.createCategory); //middleware añadido
 
     return router;
   }
