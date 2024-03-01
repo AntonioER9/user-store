0d121ed3404ceb98ffef2bb2c853dd43e015f048
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-03-01 10:31:29 -0300

    add seed for populate the database

--- src/data/mongo/mongo-database.ts
+++ src/data/mongo/mongo-database.ts
@@ -1,30 +1,35 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 
 
 interface Options {
-    mongoUrl: string;
-    dbName: string;
+  mongoUrl: string;
+  dbName: string;
 }
 
+
 export class MongoDatabase {
-    static async connect(options: Options){
 
-        const { mongoUrl, dbName } = options;
+  static async connect( options: Options ) {
+    const { mongoUrl, dbName } = options;
 
-        try {
-            
-            await mongoose.connect(mongoUrl, {
-                dbName: dbName,
-            })
-            
-            return true;
+    try {
+      await mongoose.connect( mongoUrl, {
+        dbName: dbName,
+      });
 
-        } catch (error) {
-            console.log('Mongo connection error');
-            throw error;
-        }
+      return true;
 
-
+    } catch (error) {
+      console.log('Mongo connection error');
+      throw error;
     }
+
+  }
+
+  static async disconnect() {
+    await mongoose.disconnect();
+  }
+
+
 }
